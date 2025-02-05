956add530cc3b8d767d2f3c07d0be01bec7a9bd7
Liz <lizrd2016@gmail.com>
2025-02-04 18:47:34 -0800

    Add middleware code for login functionality

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -3,7 +3,7 @@
 // the dependencies
 const bcrypt = require("bcryptjs");
 const jsonWebToken = require("jsonwebtoken");
-const {client} = require("pg");
+const {Client} = require("pg");
 
 // set up connection to the database
 const client = new Client({
@@ -15,29 +15,32 @@ const client = new Client({
 
 client.connect();
 
-const loginUser = async(request, response) => {
-    const {email, password} = request.body;
+const loginUser = async(req, res) => {
+    const {email, password} = req.body;
 
     try {
         // compare the email provided with the database
-        const sqlQuery = await sql`SELECT * FROM users WHERE email = ${email}`;
-        if (sqlQuery.length === 0) {
-            return response.status(400).json({error: "Invalid Credentials"}); // HTTP/1.1 400 Bad Request
+        const sqlQuery = await client.query("SELECT * FROM users WHERE email = $1", [email]);
+        if (sqlQuery.rows.length === 0) {
+            return res.status(400).json({error: "Invalid Credentials"}); // HTTP/1.1 400 Bad Request
         }
 
         // check the password
         const user = sqlQuery[0];
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
-            return response.status.json({error: "Invalid Credentials"});
+            return res.status(400).json({error: "Invalid Credentials"});
         }
 
         // generate a token for the user that will expire (security measure)
-        const token = jsonWebToken.sign({userId: user.user_id}, process.env.jsonWebToken_SECRET, {expiresIn: "1hr"});
-        response.json({token});
+        const token = jsonWebToken.sign(
+            {userId: user.user_id},
+            process.env.JWT_SECRET,
+            {expiresIn: "1h"});
+        res.json({token});
     } catch (error) {
         console.error(error);
-        response.status(500).json({error: "Internal Server Error"}); // HTTP/1.1 500 Internal Server Error
+        res.status(500).json({error: "Internal Server Error"}); // HTTP/1.1 500 Internal Server Error
     }
 }
 
--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -1,1 +1,27 @@
 // Verify JSON Web Tokens
+
+// the JSON Web Token dependency
+const jsonWebToken = require("jsonwebtoken");
+
+const authenticatedUser = (req, res, next) => {
+    // grab the token in the request header
+    const token = req.header("Authorization");
+    // if there is no token, return a 401 status code & error message
+    if (!token) {
+        return res.status(401).json({error: "Access denied. No token provided"}) // HTTP/1.1 401 Unauthorized
+    }
+
+    // verify the token
+    try {
+        // see if the token matches what is in the .env file
+        const decoded = jsonWebToken.verify(token.replace("Bearer ", ""), process.env.jsonWebToken_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({error: "Invalid token"});
+    }
+};
+
+// export for use elsewhere in the application
+module.exports = {authenticatedUser};
