f02fc8ac4b6407d15b03407159e01f5c80a4f87e
kangnade <kangnade@gmail.com>
2025-02-05 12:47:37 -0800

    Created component LoginBody.js that includes the login boxes for user to log in. Created LoginPage.js that reuses the Navbar and Footer components, and include the LoginBody component to display as a login page.

---
+++ frontend/src/LoginPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import LoginBody from "./components/LoginBody";
+
+const LoginPage = () => {
+  return (
+    <>
+      <Navbar />
+      <LoginBody />
+      <Footer />
+    </>
+  );
+};
+
+export default LoginPage;
