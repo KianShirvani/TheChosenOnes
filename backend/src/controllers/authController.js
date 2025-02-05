fedaa104ad9051fefd3c0dd4db1e9d6fe75a7817
Liz <lizrd2016@gmail.com>
2025-02-04 20:25:01 -0800

    Add tests for login functionality

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -13,34 +13,40 @@ const client = new Client({
     }
 });
 
-client.connect();
+// to prevent connecting to the database during tests
+if (process.env.NODE_ENV !== "test") {
+    client.connect();
+}
+
 
 const loginUser = async(req, res) => {
     const {email, password} = req.body;
 
     try {
+        // if no email or no password, the login attempt is invalid
+        if (!email || !password) {
+            return res.status(400).json({error: "Missing fields"});
+        }
         // compare the email provided with the database
         const sqlQuery = await client.query("SELECT * FROM users WHERE email = $1", [email]);
-        if (sqlQuery.rows.length === 0) {
+        if (!sqlQuery.rows || sqlQuery.rows.length === 0) {
             return res.status(400).json({error: "Invalid Credentials"}); // HTTP/1.1 400 Bad Request
         }
 
         // check the password
-        const user = sqlQuery[0];
+        const user = sqlQuery.rows[0];
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return res.status(400).json({error: "Invalid Credentials"});
         }
 
         // generate a token for the user that will expire (security measure)
-        const token = jsonWebToken.sign(
-            {userId: user.user_id},
-            process.env.JWT_SECRET,
-            {expiresIn: "1h"});
-        res.json({token});
+        const token = jsonWebToken.sign({userId: user.user_id}, process.env.JWT_SECRET, {expiresIn: "1h"});
+
+        return res.status(200).json({token});
     } catch (error) {
-        console.error(error);
-        res.status(500).json({error: "Internal Server Error"}); // HTTP/1.1 500 Internal Server Error
+        console.error("error in loginUser:", error);
+        return res.status(500).json({error: "Internal Server Error"}); // HTTP/1.1 500 Internal Server Error
     }
 }
 
