6bbd365441179ee2c39a53ebe0574cc51c1cadfe
Kian Shirvani <kian.shirvani@outlook.com>
2025-02-05 17:04:57 -0800

    Update app.js after merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';  // 引入 Header 组件
+import React from "react";
+import "./App.css";
+import Header from "./components/Header"; 
+import Navbar from "./components/Navbar"; 
+import HeroSection from "./components/HeroSection";
+import FeaturesSection from "./components/FeaturesSection"; 
+import Testimonials from "./components/Testimonials"; 
+import CallToAction from "./components/CallToAction";
+import Footer from "./components/Footer"; 
 
 function App() {
   return (
     <div className="App">
-      <Header />  {/* 添加 Header 组件 */}
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header /> 
+      <HeroSection /> 
+      <FeaturesSection />
+      <Testimonials />
+      <CallToAction />
+      <Footer />
     </div>
-  ); 
+  );
 }
 
 export default App;
-
