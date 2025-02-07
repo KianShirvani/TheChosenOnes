34e74f0b570e1ebdde1b7f8bd20e7d3574ef88bb
kangnade <kangnade@gmail.com>
2025-02-07 12:48:36 -0800

    added the navbar files. still needs to fix the routing to login page

---
+++ frontend/src/components/LoginBody.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+
+
+
+const LoginBody = () => {
+ return(
+   <div style={styles.container}>
+     <h2>User Login</h2>
+     <div>
+       <Input type="text" placeholder="Username or Email" style={styles.input}/>
+       <Input type="password" placeholder="Password" style={styles.input}/>
+       <Button style={styles.button}>Login</Button>
+     </div>
+   </div>
+ );
+};
+
+
+const styles = {
+ container: {
+   display: "flex",
+   flexDirection: "column",
+   alignItems: "center",
+   justifyContent: "center",
+   minHeight: "100vh",
+   padding: "1rem",
+ },
+ title: {
+   fontSize: "1.5rem",
+   fontWeight: "bold",
+   marginBottom: "1rem",
+ },
+ form: {
+   width: "100%",
+   maxWidth: "20rem",
+   display: "flex",
+   flexDirection: "column",
+   gap: "1rem",
+ },
+ input: {
+   width: "100%",
+   padding: "0.5rem",
+   border: "1px solid #ccc",
+   borderRadius: "0.5rem",
+ },
+ button: {
+   width: "100%",
+ },
+};
+
+
+export default LoginBody
