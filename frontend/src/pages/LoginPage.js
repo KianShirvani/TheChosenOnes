3a11ab8688c68a00767b78060da342cd97df8eb0
Liz <lizrd2016@gmail.com>
2025-02-19 12:50:24 -0800

    Reorganize frontend files

---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LoginBody from "../components/LoginBody";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+const LoginPage = () => {
+  return (
+    <>
+      <LoginBody />
+    </>
+  );
+};
+
+export default LoginPage;
---
+++ frontend/src/pages/MainPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import HeroSection from "../components/HeroSection";
+import FeaturesSection from "../components/FeaturesSection";
+import Testimonials from "../components/Testimonials";
+import CallToAction from "../components/CallToAction";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+const MainPage = () => {
+  return (
+    <>
+      <HeroSection />
+      <FeaturesSection />
+      <Testimonials />
+      <CallToAction />
+    </>
+  );
+};
+
+export default MainPage;
