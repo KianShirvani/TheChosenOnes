ed343f921c07427bb22d664fd860b825606c8d1f
Rheiley <rheileyzoom@gmail.com>
2025-03-01 10:20:36 -0800

    Update github actions

--- backend/src/tests/databaseInitialization.test.js
+++ backend/src/tests/databaseInitialization.test.js
@@ -7,7 +7,7 @@ describe('Database Initialization', () => {
   beforeAll(async () => {
     client = new Client({
       user: 'postgres',
-      host: '127.0.0.1',
+      host: process.env.LOCALHOST,
       database: 'mydatabase',
       password: 'password',
       port: 5432,
