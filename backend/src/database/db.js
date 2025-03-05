028cee6c726ecb38fc9862e0d4b5dccc6decc36d
Kian Shirvani <kian.shirvani@outlook.com>
2025-03-05 14:13:48 -0800

    update db.js

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -1,13 +1,17 @@
-const { Pool } = require("pg");
+const pg = require("pg");
 require("dotenv").config(); // Ensure environment variables are loaded
 
-const connectionString = process.env.DATABASE_URL || ""; // Ensure it's not undefined
+if (!process.env.DATABASE_URL) {
+  console.error("DATABASE_URL is not defined. Check your .env file.");
+  process.exit(1);
+}
+
+const { Pool } = pg;
+const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
   connectionString,
-  ssl: connectionString.includes("localhost")
-    ? false
-    : { rejectUnauthorized: false }, // Use SSL only if it's not localhost
+  ssl: connectionString.includes("localhost") ? false : { rejectUnauthorized: false }
 });
 
 module.exports = pool;
