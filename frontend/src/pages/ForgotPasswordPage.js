0c50617058359a5588a08f08d54762eec1d1bd6f
Liz <lizrd2016@gmail.com>
2025-03-28 17:28:17 -0700

    Add CSS for 'forgot-password' page

--- frontend/src/pages/ForgotPasswordPage.js
+++ frontend/src/pages/ForgotPasswordPage.js
@@ -17,21 +17,89 @@ const ForgotPasswordPage = () => {
   };
 
   return (
-    <div>
-      <h2>Forgot Password</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Enter your email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <button type="submit">Send Reset Link</button>
+    <div style={styles.container}>
+      <h2 style={styles.title}>Forgot Password</h2>
+      <form onSubmit={handleSubmit} style={styles.form}>
+      <label style={styles.label}>
+          <span style={styles.spanLabel}>Email</span>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            style={styles.input}
+          />
+        </label>
+        <button type="submit" style={styles.button}>Send Reset Link</button>
       </form>
-      {message && <p>{message}</p>}
+      {message && <p style={styles.link}>{message}</p>}
     </div>
   );
+};
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    minHeight: "70vh",
+    padding: "1rem",
+    marginTop: "100px"
+  },
+  title: {
+    fontSize: "1.8rem",
+    fontWeight: "bold",
+    marginBottom: "1rem",
+  },
+  form: {
+    width: "100%",
+    maxWidth: "20rem",
+    display: "flex",
+    flexDirection: "column",
+    gap: "1rem",
+  },
+  label: {
+    position: "relative",
+    display: "block",
+    width: "100%",
+  },
+  spanLabel: {
+    position: "absolute",
+    left: "10px",
+    top: "50%",
+    transform: "translateY(-50%)",
+    color: "#000",
+    fontSize: "16px",
+    transition: "all 0.3s ease",
+    pointerEvents: "none",
+  },
+  input: {
+    width: "95%",
+    padding: "0.5rem",
+    background: "transparent",
+    border: "none",
+    borderRadius: 0,
+    borderBottom: "2px solid #7000da",
+    transition: "all 0.3s ease-in-out",
+    outline: "none",
+  },
+  button: {
+    width: "100%",
+    backgroundColor: "#7000da",
+    color: "white",
+    marginTop: "10px",
+    padding: "0.75rem",
+    border: "none",
+    borderRadius: "0.5rem",
+    cursor: "pointer",
+    transition: "0.3s",
+    boxShadow: "0 0 10px rgba(112, 0, 218, 1)",
+  },
+  link: {
+    marginTop: "1rem",
+    fontSize: "0.9rem",
+    color: "#7000da",
+  },
 };
 
 export default ForgotPasswordPage;
