c9a1700276a1851fd77adfd4da30f04c807676cb
Kian Shirvani <kian.shirvani@outlook.com>
2025-03-30 17:22:40 -0700

    improve formatting

--- frontend/src/pages/AdminManagement.js
+++ frontend/src/pages/AdminManagement.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../components/ui/button";
 import { motion } from "framer-motion";
+
 const AdminManagement = () => {
     const navigate = useNavigate();
     const [users, setUsers] = useState([]);
